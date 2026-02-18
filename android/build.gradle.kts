88a77b2310edaedbea5a47952982a408b566ee2a
ChamikaraD <115696292+ChamikaraD@users.noreply.github.com>
2026-02-19 01:07:51 +0530

    Firebase integrated and build fixed

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,20 +5,8 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
+// IMPORTANT: Restore default Flutter build directory behavior
+rootProject.buildDir = File(rootProject.projectDir.parentFile, "build")
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+    project.buildDir = File(rootProject.buildDir, project.name)
 }

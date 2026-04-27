652d703ecf33ac683be6b289e247e203ae6550a7
JanithaDissanayaka <janithadissnayakaa@gmail.com>
2026-04-28 00:12:15 +0530

    add Screen to edit an existing summary

--- lib/features/notes/screens/edit_summary_screen.dart
+++ lib/features/notes/screens/edit_summary_screen.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 /// Screen to edit an existing summary
+/// NOTE: This screen is currently only used for summaries, not notes. The Firestore path is hardcoded to 'summaries'.
 class EditSummaryScreen extends StatefulWidget {
   final String docId;
   final String title;
